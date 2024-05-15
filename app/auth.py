e1012f74921705be20d49b0804665adec4fa5379
palochs1 <159847316+palochs1@users.noreply.github.com>
2024-05-16 02:05:12 +0700

    edit auth.py

--- app/auth.py
+++ app/auth.py
@@ -5,6 +5,10 @@ from app.models import User
 from app import db
 import random
 import string
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 
 bp = Blueprint('auth', __name__)
@@ -18,6 +22,9 @@ def login():
     user = User.query.filter_by(username=username).first()
     if user and check_password_hash(user.password, password):
         access_token = create_access_token(identity=username)
+
+        send_otp_email(user.email, user.otp)
+
         return jsonify(access_token=access_token)
     return jsonify({"msg": "Bad username or password"}), 401
 
@@ -49,8 +56,40 @@ def verify_otp():
 
         # Compare the OTP entered by the user with the stored OTP
         if user_input_otp == stored_otp:
-            return jsonify({"msg": "OTP verified successfully."}), 200
+            access_token = create_access_token(identity=username)
+            return jsonify({"access_token": access_token}), 200
         else:
             return jsonify({"msg": "Invalid OTP. Please try again."}), 400
     else:
         return jsonify({"msg": "User not found."}), 404
+    
+def send_otp_email(email, otp):
+    # Email configuration for Gmail
+    sender_email = "paloch54@gmail.com"
+    sender_password = "bsgm nvgc xjzr erhe"
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    
+    # Create message container
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = email
+    msg['Subject'] = "OTP Verification"
+    
+    # Email body
+    body = f"Your OTP for verification is: {otp}"
+    msg.attach(MIMEText(body, 'plain'))
+    
+    # Establish a connection to the SMTP server
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    
+    # Login to the SMTP server
+    server.login(sender_email, sender_password)
+    
+    # Send the email
+    server.sendmail(sender_email, email, msg.as_string())
+    
+    # Close the connection
+    server.quit()
+
